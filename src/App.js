6cc89f372a141b65c476fe2a523612bfde6d21ca
Fesal <mkircal957@gmail.com>
2021-12-08 14:15:02 +0300

    App.js Değişikliği
    +Learn React yazısı Mehmet Kırçal olarak değiştirildi.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mehmet Kırçal
         </a>
       </header>
     </div>
